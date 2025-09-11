6e6dc26c16c162b1c30cd9a4ab0b5c8b9e5dabd7
cirivere <147147650+cirivere@users.noreply.github.com>
2025-09-11 15:10:41 +0200

    Update script.js
    Adding in the checkbox values for the save and load data. it does not work and returns NULL, I need to define this somehow???

--- script.js
+++ script.js
@@ -300,9 +300,10 @@ function loadInputData() {
     tribeState = JSON.parse(localStorage["AlliedSocietyFFXIV"]);
   }
   for (var tribe of Object.keys(tribeState)) {
-    
+    var tribeSelectId = "Count_" + tribe;
     var tribeRankId = tribe + "_rank";
     var tribeRepId = tribe + "_current_rep";
+    document.getElementById(tribeSelectId).value = tribeState[tribe]["select"];   
     document.getElementById(tribeRankId).value = tribeState[tribe]["rank"];
     document.getElementById(tribeRepId).value = tribeState[tribe]["rep"];
   }
@@ -315,10 +316,12 @@ function saveInputData() {
 
   for (var tribeElement of allTribes) {
     var tribe = tribeElement.id;
+    var tribeSelectId = "Count_" + tribe;
     var tribeRankId = tribe + "_rank";
     var tribeRepId = tribe + "_current_rep";
 
     tribeState[tribe] = {
+      "select": document.getElementById(tribeSelectId).value,
       "rank": document.getElementById(tribeRankId).value,
       "rep": document.getElementById(tribeRepId).value,
     }
